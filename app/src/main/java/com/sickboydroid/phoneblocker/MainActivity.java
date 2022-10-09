411414987e9d9c4bf5104cae8bcab2f6ab65c71c
sickboydroid <sickboydroid@gmail.com>
2022-10-09 11:42:52 +0530

    Initial commit

---
+++ app/src/main/java/com/sickboydroid/phoneblocker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sickboydroid.phoneblocker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
